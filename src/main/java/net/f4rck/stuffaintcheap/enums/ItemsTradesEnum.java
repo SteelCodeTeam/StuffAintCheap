36f6fd338849266614c2f324637cdb33a2775c4c
F4rck <140993687+F4rck@users.noreply.github.com>
2023-08-09 21:10:20 -0600

    Oops

--- src/main/java/net/f4rck/stuffaintcheap/enums/ItemsTradesEnum.java
+++ src/main/java/net/f4rck/stuffaintcheap/enums/ItemsTradesEnum.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.Items;
 
 public class ItemsTradesEnum {
     public enum ItemsTier1 {
-        BOOK(new ItemStack(Items.BOOK, 4), new ItemStack(Items.EMERALD, 1),  12,5),
+        BOOK(new ItemStack(Items.BOOK, 1), new ItemStack(Items.EMERALD, 1),  12,5),
         PAPER(new ItemStack(Items.PAPER, 24), new ItemStack(Items.EMERALD, 1),  16,5),
         INK_SAC(new ItemStack(Items.INK_SAC, 5), new ItemStack(Items.EMERALD, 1),  12,5),
         WRITABLE_BOOK(new ItemStack(Items.WRITABLE_BOOK, 1), new ItemStack(Items.EMERALD, 1),  12,5),
@@ -45,10 +45,10 @@ public class ItemsTradesEnum {
 
 
     public enum ItemsTier3 {
-        NAME_TAG(new ItemStack(Items.NAME_TAG, 1), new ItemStack(Items.EMERALD, 20),  12, 30),
-        GLASS(new ItemStack(Items.GLASS, 4),  new ItemStack(Items.EMERALD, 1),  16, 10),
-        CLOCK(new ItemStack(Items.CLOCK, 1),  new ItemStack(Items.EMERALD, 1),  12, 20),
-        COMPASS(new ItemStack(Items.COMPASS, 1),  new ItemStack(Items.EMERALD, 1),  12, 20);
+        NAME_TAG( new ItemStack(Items.EMERALD, 20), new ItemStack(Items.NAME_TAG, 1),  12, 30),
+        GLASS( new ItemStack(Items.EMERALD, 1), new ItemStack(Items.GLASS, 4),  16, 10),
+        CLOCK( new ItemStack(Items.EMERALD, 1), new ItemStack(Items.CLOCK, 1),  12, 20),
+        COMPASS( new ItemStack(Items.EMERALD, 1), new ItemStack(Items.COMPASS, 1),  12, 20);
 
 
         private final ItemStack itemCost;
